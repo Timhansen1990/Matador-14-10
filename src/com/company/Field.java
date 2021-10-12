2bc0235001370048db090e3add46df6603a1b38d
Martin Rohde <mart3b24@edu.easj.dk>
2021-10-12 14:09:24 +0200

    Update Field.java

--- src/com/company/Field.java
+++ src/com/company/Field.java
@@ -7,6 +7,6 @@ public class Field {
     // Feldt navn
     static String[] field = {"Rådhuspladsen", "Åkandevej", "Bistandskontoret", "Ved  postkassen", "András hjem", "Det lokale bodega", "AA mødet", "Field of dreams", "Strandet", "Java district"};
     // Feldt pris
-    static int[] fieldPrice = {100, 100, 100, 100, 100, 100, 100, 10000, 100, 100};
+    static int[] fieldPrice = {100, 100, 100, 100, 10, 100, 100, 10000, 100, 100};
 
 }
