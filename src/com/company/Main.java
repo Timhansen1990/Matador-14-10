185caf6c1401a7d63da3adfb8d5c548232220456
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2021-10-13 12:31:13 +0200

    Update Main.java
    kode flyttet til user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Users.writeUser();
+        Bank.Purchase(0,1);
     }
 }
